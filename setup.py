7d52c7dea4ccd981cf706300607f836a975a99fd
Dharmikkumar Anaghan <98855357+dharmikkuma@users.noreply.github.com>
2024-02-27 20:33:16 +0100

    long description

--- setup.py
+++ setup.py
@@ -15,13 +15,19 @@ def get_requirements(file_path:str)-> List[str]:
 
     return requirements
 
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name="Python_calculator", # name of your Pipy package
-    version="0.0.1",
+    version="0.0.2",
     author="Dharmikkumar Anaghan",
     author_email="dharmikanghan1996@gmail.com",
     url="https://github.com/dharmikkuma/First_Package.git",
     description="An application that informs you of the time in different locations and timezones",
+    long_description=long_description,
+    long_description_content="text/markdown",
     install_requires= ["numpy","flake8", "pytest","tox","mypy>=0.971","black>=22.8.0","tox-gh-actions","pathlib"],
     package_dir = {"": "src"},
     packages=find_packages(where = "src"),

f8c97a9dbd5defb85988930d14be59c1a996f1c8
Dharmikkumar Anaghan <98855357+dharmikkuma@users.noreply.github.com>
2024-02-27 18:17:35 +0100

    file uploaded

--- setup.py
+++ setup.py
@@ -1,17 +1,26 @@
 from setuptools import setup, find_packages
+from typing import List
+
+HYPEN_E_DOT = "-e ."
+def get_requirements(file_path:str)-> List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
 
 setup(
-    name="Python_Package",
+    name="Python_calculator", # name of your Pipy package
     version="0.0.1",
     author="Dharmikkumar Anaghan",
     author_email="dharmikanghan1996@gmail.com",
-    url="https://github.com/RekhuGopal/pythoncustompackages.git",
+    url="https://github.com/dharmikkuma/First_Package.git",
     description="An application that informs you of the time in different locations and timezones",
-    packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=["click", "pytz"],
-    entry_points={"console_scripts": ["Python_Package = src.main:main"]},
-)
+    install_requires=get_requirements("./requirements.txt"),
+    package_dir = {"": "src"},
+    packages=find_packages(where = "src"),
+    )
--- src/logic.py
+++ src/logic.py
@@ -1,53 +1,27 @@
-from datetime import datetime
-import pytz
+import numpy as np  
 
-def area(location):
-    """This function takes in a location as argument, checks the list of locations available and returns the formatted time to the user."""
-    location = format_location(location)
-    for areas in pytz.all_timezones:
-        if location.lower() in areas.lower():
-            location = areas
-            tz = pytz.timezone(location)
-            date_now = datetime.now(tz)
-            formatted_date = date_now.strftime("%B %d, %Y %H:%M:%S")
-            print(f"{location} time: ", formatted_date)
-            break
 
-    else:
-        print("This location isn't on the tz database on Wikipedia")
+class calculator():
+    
+    def sum(a, b):
+        answer = a + b
 
-def area_zone(zone):
-    """This function takes in a time zone as argument, checks the list of timezones and returns the formatted time to the user."""
-    try:
-        zone = timezones(zone)
-        tz = pytz.timezone(zone)
-        date_now = datetime.now(tz)
-        formatted_date = date_now.strftime("%B %d, %Y %H:%M:%S")
-        print(f"{zone} time: ", formatted_date)
+        return answer
+    
+    def mul(a, b):
+        answer = a * b
 
-    except Exception:
-        print("Timezone is not on the list. Consider using location instead.")
+        return answer
 
-def timezones(zone):
-    """This function is used to handle situations of Daylight Saving Time that the standard library can't recognize."""
-    zones = {
-        "PDT": "PST8PDT",
-        "MDT": "MST7MDT",
-        "EDT": "EST5EDT",
-        "CDT": "CST6CDT",
-        "WAT": "Etc/GMT+1",
-        "ACT": "Australia/ACT",
-        "AST": "Atlantic/Bermuda",
-        "CAT": "Africa/Johannesburg",
-    }
+    def dev(a, b):
+        if b == 0:
+            return print("devision by zero")
+        answer = a/ b
 
-    try:
-        zones[zone]
+        return answer
+    
+    def sub(a, b):
+        answer = a - b
 
-    except:
-        return zone
-    return zones[zone]
-
-def format_location(location):
-    location = location.replace(" ", "_")
-    return location
+        return answer
+    

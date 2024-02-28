74e63dd2292b6f12d6b5f78e8213f326bc7c7717
Dharmikkumar Anaghan <98855357+dharmikkuma@users.noreply.github.com>
2024-02-28 16:07:38 +0100

    setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from typing import List
 from pathlib import Path
 
-with open('README.md', 'r', encoding='utf-8') as f:
+with open('README.md') as f:
     long_description = f.read()
 
 setup(
